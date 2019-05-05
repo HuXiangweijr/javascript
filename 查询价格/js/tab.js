6c4c34b7bc48ced48e85ed51b243977d223da059
攻城狮 <44216780+HuXiangweijr@users.noreply.github.com>
2019-05-05 17:38:01 +0800

    Add files via upload

---
+++ 查询价格/js/tab.js
@@ -0,0 +1,29 @@
+// window.onload = function () {
+    // Tab栏封装
+    function Tab(obj, title, content) {
+        // 获取父级id
+        var target = document.getElementById(obj);
+        // Tab栏切换标题
+        var one = target.children[0].children;
+        // Tab栏切换内容
+        var two = target.children[1].children;
+        // 第一遍循环，将Tab栏标题拿出
+        for (var i = 0; i < one.length; i++) {
+            // 获取Tab栏标题的索引号
+            one[i].index = i;
+            // 当鼠标点击时，执行
+            one[i].onclick = function () {
+                // 第二遍循环
+                for (var j = 0; j < one.length; j++) {
+                    // 将所有的class名清空
+                    one[j].className = "";
+                    two[j].className = "";
+                }
+                // 为当前点击的class添加类名
+                this.className = title;
+                // 将Tab栏的标题和内容，通过索引号绑定，并给内容添加class
+                two[this.index].className = content;
+            }
+        }
+    }
+// }
